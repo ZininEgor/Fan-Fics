e19f1756f1f734fc2ec1dd28e81d54f7ef90e6ae
ZininEgor <zininegor00@gmail.com>
2021-07-12 16:08:18 +0300

    fix navbar links

--- client/src/components/ReadFanfic.js
+++ client/src/components/ReadFanfic.js
@@ -280,11 +280,13 @@ export default function DetailMyFanfic() {
                                             >
                                                 Комментарии
                                             </p>
-                                            <div
-                                                className="flex mx-auto items-center justify-center mb-4 max-w-2xl">
-                                                <div className="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
-                                                    <div className="flex flex-wrap -mx-3 mb-6">
-                                                        <div className="w-full md:w-full px-3 mb-2 mt-2">
+                                            {
+                                                auth.isAuthenticated &&
+                                                <div
+                                                    className="flex mx-auto items-center justify-center mb-4 max-w-2xl">
+                                                    <div className="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+                                                        <div className="flex flex-wrap -mx-3 mb-6">
+                                                            <div className="w-full md:w-full px-3 mb-2 mt-2">
                                                             <textarea
                                                                 className="bg-gray-100 rounded border border-gray-400 place-holder-gray-50 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
                                                                 name="body"
@@ -293,45 +295,46 @@ export default function DetailMyFanfic() {
                                                                 required
                                                                 defaultValue={""}
                                                             />
-                                                        </div>
-                                                        <div
-                                                            className="w-full md:w-full flex items-start md:w-full px-3">
-                                                            <div
-                                                                className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
-                                                                <svg
-                                                                    fill="none"
-                                                                    className="w-5 h-5 text-gray-600 mr-1"
-                                                                    viewBox="0 0 24 24"
-                                                                    stroke="currentColor"
-                                                                >
-                                                                    <path
-                                                                        strokeLinecap="round"
-                                                                        strokeLinejoin="round"
-                                                                        strokeWidth={2}
-                                                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                                    />
-                                                                </svg>
-                                                                <p
-                                                                    id="first-name"
-                                                                    className="text-lime-600 dark:text-lime-400 text-sm sm:text-sm lg:text-sm xl:text-sm"
-                                                                >
-                                                                    Пожалуйста,уважайте себя и других участников
-                                                                    сообщества
-                                                                </p>
-
                                                             </div>
-                                                            <div className="-mr-1">
-                                                                <button
-                                                                    onClick={() => sendComment(comment)}
-                                                                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                                                >
-                                                                    Отправить
-                                                                </button>
+                                                            <div
+                                                                className="w-full md:w-full flex items-start md:w-full px-3">
+                                                                <div
+                                                                    className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                                                                    <svg
+                                                                        fill="none"
+                                                                        className="w-5 h-5 text-gray-600 mr-1"
+                                                                        viewBox="0 0 24 24"
+                                                                        stroke="currentColor"
+                                                                    >
+                                                                        <path
+                                                                            strokeLinecap="round"
+                                                                            strokeLinejoin="round"
+                                                                            strokeWidth={2}
+                                                                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                                                        />
+                                                                    </svg>
+                                                                    <p
+                                                                        id="first-name"
+                                                                        className="text-lime-600 dark:text-lime-400 text-sm sm:text-sm lg:text-sm xl:text-sm"
+                                                                    >
+                                                                        Пожалуйста,уважайте себя и других участников
+                                                                        сообщества
+                                                                    </p>
+
+                                                                </div>
+                                                                <div className="-mr-1">
+                                                                    <button
+                                                                        onClick={() => sendComment(comment)}
+                                                                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                                    >
+                                                                        Отправить
+                                                                    </button>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            }
 
                                         </div>
 
